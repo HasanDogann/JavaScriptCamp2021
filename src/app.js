71586b5b5c47c6c3a14d95fa8e2415d56687f3e1
HasanDogann <hasandogann38@gmail.com>
2021-10-19 02:25:39 +0300

    19.10 Lesson1

---
+++ src/app.js
@@ -0,0 +1,37 @@
+console.log("Hasan d");
+
+let dollarYesterday = 9.2;
+let dollarToday = 9.1;
+const text = document.getElementById("parag");
+const links = document.getElementById("hd");
+const list = document.getElementById("list");
+let konutKredileri = [
+  "Ev kredisi",
+  "Emlak Konut",
+  "Varyap Kredisi",
+  "Özel Konut Kredileri",
+];
+//For döngüsü ile liste oluşturma
+for (let i = 0; i < konutKredileri.length; i++) {
+  list.innerHTML += "<li>" + konutKredileri[i] + "</li>";
+}
+//Dolar kontrolü ile html içeriği değiştirme
+function dolar() {
+  if (dollarToday > dollarYesterday) {
+    text.innerHTML = " Dolar baya arttı ha";
+    links.innerHTML = "Yurtdışında iş bul ";
+    links.href = "https://www.glassdoor.com";
+    links.target = "_blank";
+    links.hasan = "new1";
+  } else if (dollarToday < dollarYesterday) {
+    text.innerHTML = " Dolar azaldı ,çok ilginç";
+    links.innerHTML = "Yurtdışında iş bul ama acil değil ";
+    links.href = "https://www.indeed.com";
+    links.target = "_blank";
+    links.hasan = "new2";
+  } else {
+    text.innerHTML = " Dolar sabit, iyi bari";
+  }
+}
+
+dolar();
